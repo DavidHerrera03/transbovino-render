b0a217af98848a2c1d37e93e8799ebcf76f8ad5c
David <davidstevenherrerarincon@gmail.com>
2026-04-29 18:53:01 -0500

    Agregar longitudes a columnas String para MySQL

--- backend/models/finca.py
+++ backend/models/finca.py
@@ -1,12 +1,13 @@
 from sqlalchemy import Column, Integer, String, ForeignKey
 from database import Base
 
+
 class Finca(Base):
     __tablename__ = "finca"
 
     id_finca = Column(Integer, primary_key=True, index=True, autoincrement=True)
-    municipio = Column(String, default="Zipaquira")
-    vereda = Column(String)
-    referencia = Column(String)
-    nombre_finca = Column(String)
+    municipio = Column(String(120), default="Zipaquira")
+    vereda = Column(String(120))
+    referencia = Column(String(255))
+    nombre_finca = Column(String(150))
     id_usuario = Column(Integer, ForeignKey("usuarios.id_usuario"))
